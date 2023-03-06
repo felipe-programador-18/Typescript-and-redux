38f3f08b2912f6c70ee3fda228c9a3b2b6eba21d
Felipe <machadofelipe2016@outlook.com>
2023-03-06 11:33:45 -0300

    c7-to create my action creaters

--- redux-ts/src/state/Actions-Creaters/TesteAction.tsx
+++ redux-ts/src/state/Actions-Creaters/TesteAction.tsx
@@ -1,25 +1,38 @@
 import axios from "axios"
-
+import { Dispatch } from "redux"
 import { Action } from "../Action/ActionManage"
 import { ActionType } from "../Actions-type/Actions-type"
 
-const SearchRepositories = (term:string) => {
+export const SearchRepositories = (term:string) => {
   
-    return async (dispatch: any) => {
+    return async (dispatch: Dispatch<Action>) => {
       dispatch({
         type: ActionType.SEARCH_REPOSITORIES
       })
 
       try {   
-      } catch (error:any) {
-       dispatch({
-        type: ActionType.SEARCH_REPOSITORIES_ERROR,
-        payload: error.message
-       })
+        const {data} = await axios.get('https://registry.npmjs.org/-/v1/search', {
+            params: {
+             text: term
+            }
+        })
+
+       const AddDates = data.objects((dates:any) => {
+            return  dates.package.name
+        })
+        
+        dispatch({
+            type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+            payload: AddDates
+        })
+       
+      }catch (error:any) {
+        dispatch({
+         type: ActionType.SEARCH_REPOSITORIES_ERROR,
+         payload: error.message
+        })
       }
     
     
     }
 }
-
-export default SearchRepositories
