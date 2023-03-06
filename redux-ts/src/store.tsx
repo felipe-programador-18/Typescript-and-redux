d916fe9ad7c44997f9c97856a2743bf5a7eed525
Felipe <machadofelipe2016@outlook.com>
2023-03-06 12:23:30 -0300

    c10-to deleted my store

--- redux-ts/src/store.tsx
+++
@@ -1,6 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import thunk from 'redux-thunk'
-
-import reducers from "./state/RepositoReducer/Repositories.reducers"
-
-export const store = createStore(reducers, {},applyMiddleware(thunk))
