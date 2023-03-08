daf6cf27f7f531de63e32a79b28166bf8e67ae88
Felipe <machadofelipe2016@outlook.com>
2023-03-08 10:38:51 -0300

    c15-to create another hook personality

---
+++ redux-ts/src/Hooks/useActions.ts
@@ -0,0 +1,11 @@
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "../state";
+
+
+// personality hook
+export const useAction = () => {
+   const dispatch = useDispatch()
+   return bindActionCreators(actionCreators, dispatch)
+
+}
